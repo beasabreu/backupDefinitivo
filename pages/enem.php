520aecaf6dc02c6fc6ef7ef2d5cf7a8e77c3ecf6
Sungwon Yoon <03judy@naver.com>
2020-06-16 18:07:58 -0300

    Add files via upload

--- pages/enem.php
+++ pages/enem.php
@@ -1,12 +1,12 @@
 ﻿<?php
 
 
-$submenu = "<a href=\"#\">Informações</a><br>
+$submenu = "<a href=\"#info\">Informações</a><br>
 
 
-<a href=\"#\">&nbsp&nbsp&nbsp&nbspEnem Digital</a><br>
+<a href=\"#\">Enem Digital　　</a><br>
 
-<a href=\"#\">&nbsp&nbsp&nbsp&nbspEnem Impresso</a><br>
+<a href=\"#\">Enem Impresso　　</a><br>
 
 <a href=\"#\">Calendário</a><br>
 
@@ -14,7 +14,7 @@ $submenu = "<a href=\"#\">Informações</a><br>
 <a href=\"#\">Provas Anteriores</a><br>";
 
 $title = "Exame Nacional de Ensino Médio";
-$content = "nothing"; 
+$content = "<section id=\"info\">aa</section>"; 
 
 include('template.php');
 
--- pages/unitau.php
+++ pages/unitau.php
@@ -1,6 +1,6 @@
 ﻿<?php
 
-$submenu = "";
+$submenu = "<a href=\"#\">Cursos</a><br><a href=\"#\">Campus</a><br><a href=\"#\">Vestibular</a><br><a href=\"#\">Calendário</a><br><a href=\"#\">Notas de corte</a><br><a href=\"#\">Obras Literárias</a><br><a href=\"#\">SISU</a><br><a href=\"#\">Provas Anteriores</a>";
 $title = "Universidade de Taubaté";
 $content = "null"; 
 
