5eab86a56a2fce9929abd4fa49c9e487d7046f5f
Sungwon Yoon <03judy@naver.com>
2020-06-17 01:20:50 -0300

    Add files via upload

--- pages2/calendario.php
+++ pages2/calendario.php
@@ -1,9 +1,125 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
 
-$submenu = "";
-$title = "Calendário";
-$content = "aa";
+<head>
+	<meta charset="utf-8">
+	<title>FSI</title>
 
-include('template.php');
+	<!-- CSS Style -->
+	<link rel="stylesheet" type="text/css" href="../reset.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 
-?>
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="../images/favicon.ico"/>
+
+
+
+</head>
+
+
+<body>
+	<!-- skip nav -->
+	<div id="skip">
+		<a href="#header_1">Header Principal</a>		
+		<a href="#header_2">Header Secundário</a>
+		<a href="#nav_1">Menu Principal</a>
+		<a href="#nav_2">Menu Secundário</a>
+		<a href="#contents">Conteúdo</a>
+	</div>
+	<!-- // skip nav -->
+
+	<div id="wrap">
+		
+		<!-- header_1 -->
+		<div id="header_1">
+			<div>
+				<div class="header_1-menu">
+					<a href="https://github.com/swy-326/web3.0">Link_1</a>
+					<a href="http://fakeupdate.net/wnc/">Link_2</a>
+				</div>
+				<!-- // header_1 menu -->
+				<div class="header_1-title">
+					<div class="header_1-icon">
+						<a href="http://swy-326.github.io/web3.0/index.html">
+							<img src="#" width="70" height="70"/>
+						</a>
+					</div>
+					<h1>Vestibulares &amp; Universidades</h1>
+					<br>
+					<a href="https://swy-326.github.io/web3.0/">https://swy-326.github.io/web3.0/</a>
+				</div>
+				<!-- // header_1 title -->
+			</div>		
+		</div>
+		<!-- // header_1 -->
+
+		<div id="header_2">
+			<div class="header_2-container">
+				<div>
+					<p>LISTA</p>
+				</div>
+			</div>		
+			<!-- //header icon menu -->
+		</div>
+		<!-- // header_2 -->
+
+		<div id="nav_1">
+			<div class="nav-main">
+				<div>
+					<ul>						
+						<strong>CALENDÁRIO</strong>
+						<li><a href="calendario.php">CALENDÁRIO<br>GERAL</a></li>
+						<br><br><strong>UNIVERSIDADES</strong>
+						<li><a href="../pages/enem.php">ENEM</a></li>
+						<li><a href="../pages/usp.php">USP</a></li>
+						<li><a href="../pages/unicamp.php">UNICAMP</a></li>
+						<li><a href="../pages/unesp.php">UNESP</a></li>
+						<li><a href="../pages/unifesp.php">UNIFESP</a></li>
+						<li><a href="../pages/ufabc.php">UFABC</a></li>
+						<li><a href="../pages/ufscar.php">UFSCAR</a></li>
+						<li><a href="../pages/ifsp.php">IFSP</a></li>
+						<li><a href="../pages/univesp.php">UNIVESP</a></li>
+						<li><a href="../pages/ita.php">ITA</a></li>
+						<li><a href="../pages/fatec.php">FATEC</a></li>
+						<li><a href="../pages/famema.php">FAMEMA</a></li>
+						<li><a href="../pages/famerp.php">FAMERP</a></li>
+						<li><a href="../pages/uscs.php">USCS</a></li>
+						<li><a href="../unitau.php">UNITAU</a></li><br><br>
+						<strong>RANKING</strong>
+						<li><a href="rank.php">RANKING<br>GERAL</a></li>
+					</ul>
+				</div>
+			</div>		
+		</div>
+		<!-- // nav_1 -->
+		
+		<div id="contents">
+			<div class="cont">
+
+
+							<div class="cont-cont">
+				
+						<object data="index.html" width="100%" height="100%" style="float:left"></object>
+					</div>	
+				</div>
+		</div>
+		<!-- // contents -->
+
+		<div id="footer">
+			<div class="footer-container">
+				    Copyright &copy; 2020, SWY. All Rights Reserved
+			</div>
+		</div>
+		<!-- // footer -->
+
+
+	</div>
+
+
+
+</body>
+
+</html>
+
+
+
