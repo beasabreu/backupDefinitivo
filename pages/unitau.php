b61baa06d7b0b0cf2c96c44f5c66f20b8d259d95
Sungwon Yoon <sungwon.yoon@usp.br>
2020-07-04 18:51:55 -0300

    Delete unitau.php

--- pages/unitau.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-
-$submenu = "<a href=\"#\">Cursos</a><br><a href=\"#\">Campus</a><br><a href=\"#\">Vestibular</a><br><a href=\"#\">Calendário</a><br><a href=\"#\">Notas de corte</a><br><a href=\"#\">Obras Literárias</a><br><a href=\"#\">SISU</a><br><a href=\"#\">Provas Anteriores</a>";
-$title = "Universidade de Taubaté";
-$content = "null"; 
-
-include('template.php');
-
-?>
